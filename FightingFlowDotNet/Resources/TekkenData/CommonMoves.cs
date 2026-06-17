9a262b59999abefe0e447aaa16af717bec5f09b2
Dooques <samuelgdukes@gmail.com>
2026-06-17 19:19:24 +0100

    Complete styling for combo editor page

---
+++ FightingFlowDotNet/Resources/TekkenData/CommonMoves.cs
@@ -0,0 +1,17 @@
+﻿using FightingFlowDotNet.Models;
+
+namespace FightingFlowDotNet.Resources.TekkenData;
+
+public static class CommonMoves
+{
+    public static readonly List<Move> Moves =
+    [
+        new Move(name: "break", type: "Break", notation: "►", character: "Generic"),
+        new Move(name: "o_parenthesis", type: "Misc", notation: "{", character: "Generic"),
+        new Move(name: "c_parenthesis", type: "Misc", notation: "}", character: "Generic"),
+        new Move(name: "o_hold", type: "Misc", notation: "[", character: "Generic"),
+        new Move(name: "c_hold", type: "Misc", notation: "]", character: "Generic"),
+        new Move(name: "plus", type: "Misc", notation: "+", character: "Generic"),
+        new Move(name: "slide", type: "Misc", notation: "/", character: "Generic")
+    ];
+}
